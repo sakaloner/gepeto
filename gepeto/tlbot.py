8f32ce26a81f630b71f18bad4f3fbf3726ce2487
Andres Velasquez <andresf40y0@gmail.com>
2023-07-23 21:20:05 -0500

    message logging is working

--- gepeto/tlbot.py
+++ gepeto/tlbot.py
@@ -41,12 +41,34 @@ async def reply_when_mentioned(update: Update, context: ContextTypes.DEFAULT_TYP
     bot_res = chatbot.get_chatbot_response(user_msg, 0, 'public')
     await context.bot.send_message(chat_id=update.effective_chat.id, text=bot_res)
 
+def record_all_messages_decorator(func):
+    async def wrapper(update: Update, context: ContextTypes.DEFAULT_TYPE):
+        message = update.message
+        chat_id = message.chat_id
+        msg_id = message.message_id
+        text = message.text
+
+        print(f"Message ID: {msg_id}")
+        print(f"Chat ID: {chat_id}")
+        print(f"Text: {text}")
+
+        with open("recorded_messages.txt", "a") as f:
+            f.write(f"\nChat ID: {chat_id}, Message ID: {msg_id}, Text: {text}")
+
+        return await func(update, context)
+    return wrapper
+
+@record_all_messages_decorator
 async def echo_private(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message = update.message
     user_msg = update.message.text
     print(update)
     print(f'User message: {user_msg}')
     try:
         bot_res = chatbot.get_chatbot_response(user_msg, 0, 'private')
+        ## save bot_res
+        with open("recorded_messages.tx", "a") as f:
+            f.write(f"\nChat ID: {message.chat_id}, Message ID: {message.message_id+1}, Text: {bot_res}")
     except Exception as e:
         logging.error(f'error {e}', exc_info=True)
 
@@ -76,14 +98,14 @@ if __name__ == '__main__':
 
     #mentioned_filter = filters.create(is_bot_mentioned, name='MentionFilter')
 
-    #record_messages = MessageHandler(filters.TEXT & (~ filters.COMMAND) & (~ filters.Entity('mention')) & (~ filters.ChatType.Private), record_all_messages)
-    reply_when_mentioned = MessageHandler(filters.Entity('mention'), reply_when_mentioned)
+    record_messages = MessageHandler(filters.TEXT & (~ filters.COMMAND) & (~ filters.Entity('mention')) & (~ filters.ChatType.PRIVATE), record_all_messages)
+    reply_when_mentioned = MessageHandler(filters.Entity('mention') | (filters.REPLY), reply_when_mentioned)
     echo_private = MessageHandler(filters.ChatType.PRIVATE | filters.Entity('mention'), echo_private)
     start_handler = CommandHandler('gptoggle', start)
 
-    # application.add_handler(record_messages)
+    application.add_handler(record_messages)
     application.add_handler(reply_when_mentioned)
     application.add_handler(start_handler)
     application.add_handler(echo_private)
 
-    application.start_polling()
+    application.run_polling()
